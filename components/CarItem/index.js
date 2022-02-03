d297e7832b6a9d33ece453219333d882bc8d8df3
Jake P <jpetersen6202@gmail.com>
2022-02-02 19:34:54 -0600

    created and added button component

--- components/CarItem/index.js
+++ components/CarItem/index.js
@@ -1,5 +1,6 @@
 import { Text, View, ImageBackground } from "react-native";
 import styles from './styles'
+import StyledButton from "../StyledButton";
 
 const CarItem = () => {
     return (
@@ -8,13 +9,28 @@ const CarItem = () => {
             <ImageBackground
                 source={require('../../assets/images/ModelS.jpeg')}
                 style={styles.image}
-            >
-                <View style={styles.titles}>
-                    <Text style={styles.title}>Model S</Text>
-                    <Text style={styles.subtitle}>Starting at $94,990</Text>
-                </View>
-            </ImageBackground>
+            />
 
+            <View style={styles.titles}>
+                <Text style={styles.title}>Model S</Text>
+                <Text style={styles.subtitle}>Starting at $94,990</Text>
+            </View>
+
+            <StyledButton 
+                type='primary'
+                content='Custom Order'
+                onPress={() => {
+                    console.warn('custom order was pressed')
+                }}
+            />
+
+            <StyledButton 
+                type='secondary'
+                content='Existing Inventory'
+                onPress={() => {
+                    console.warn('existing inventory was pressed')
+                }}
+            />
         </View>
     );
 }
